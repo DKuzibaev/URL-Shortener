e11ad12025d72dc612d7c40c01b2300476146833
Doniyor <dkuzybaev@yandex.ru>
2025-09-09 15:51:33 +0300

    debug logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -15,8 +15,12 @@ const (
 func main() {
 	// TODO: init config: cleanenv go get -u github.com/ilyakaznacheev/cleanenv
 	cfg := config.MustLoad()
-	// TODO: init loger: slog
 
+	// TODO: init loger: slog
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 	// TODO: init storage: postgresSQL
 
 	// TODO: init router: chi, "chi render"
@@ -24,13 +28,21 @@ func main() {
 	// TODO: run server"
 }
 
-func setupLogger(env string) {
+func setupLogger(env string) *slog.Logger {
 	var log *slog.Logger
 	switch env {
 	case envLocal:
-		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
 	case envDev:
-		log := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
 	}
-
+	return log
 }
