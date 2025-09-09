e3f21d9979e1e0aabe09ba1d19a3a195da6d279d
Doniyor <dkuzybaev@yandex.ru>
2025-09-09 15:46:08 +0300

    init project

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"url-shortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	// TODO: init config: cleanenv go get -u github.com/ilyakaznacheev/cleanenv
+	cfg := config.MustLoad()
+	// TODO: init loger: slog
+
+	// TODO: init storage: postgresSQL
+
+	// TODO: init router: chi, "chi render"
+
+	// TODO: run server"
+}
+
+func setupLogger(env string) {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	}
+
+}
